14eb4325758e2e483ba4fee81b771397e4c7fc4f
Christian Marrero <90540755+cmarr049@users.noreply.github.com>
2024-01-25 22:31:45 -0500

    Add files via upload
    The inital commit of the files for this project.

---
+++ app.js
@@ -0,0 +1,10 @@
+const task = document.querySelector('#input');
+const container = document.querySelector('.container');
+
+function addTask() {
+  if (task === '') {
+    alert('Go find something to do!');
+  } else {
+    let div = document.createElement('div');
+  }
+}
